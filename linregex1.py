2d8850b4355578fc6ddca0f40f5f01f125400476
linregex1.py <philplessner@gmail.com>
2016-02-14 22:25:27 +0000

    Fit is just on training data. Added train-test split to SGD

--- linregex1.py
+++ linregex1.py
@@ -10,36 +10,41 @@ from ml_util import train_test_split
 import numpy as np
 from numpy.linalg import lstsq
 
-
+# Get the data
 Z, y = csv_reader('./data/Folds_small.csv', ['AT', 'V', 'AP', 'RH'], 'PE') 
-    
-scale = Scaler(Z)
-transform = compose(prepend_x0, Scaler.normalize)
-X = transform(scale)
-
-data = zip(X, y)
+data = zip(Z, y)
+# Split into a train set and test set
 train_data, test_data = train_test_split(data, 0.33)
-h_theta0 = [0., 0., 0., 0., 0.]
+# Scale the training data
+scale = Scaler()
+Z_train, y_train = zip(*train_data)
+scale.fit(Z_train)
+transform = compose(prepend_x0, scale.transform)
+X_train = transform(Z_train)
+scaledtrain_data = zip(X_train, y_train)
+# Scale the testing data using the same scaling parameters
+# used for the training data
+Z_test, y_test = zip(*test_data)
+X_test = transform(Z_test)
 
+h_theta0 = [0., 0., 0., 0., 0.]
+print('****Gradient Descent****')
 h_thetaf, cost = lr.fit(lr.J, 
                         lr.gradJ, 
                         h_theta0, 
                         eta=0.3, 
-                        it_max=5000, gf='gd')(train_data)
+                        it_max=5000, gf='gd')(scaledtrain_data)
 lr.plot_cost(cost)
 h_thetad = scale.denormalize(h_thetaf)
-X_train, y_train = zip(*train_data)
-X_test, y_test = zip(*test_data)
 yp_train = lr.predict(X_train, h_thetaf)
 
-print('****Training****')
+print('\n--Training--')
 print('Coefficients\t', h_thetaf)
 print(tabulate(list(zip(yp_train, y_train)), 
                 headers=['yp', 'yi'],
                 tablefmt='fancy_grid'))
     
 
-print('******************')
 print('Coefficients\t', h_thetad)
 for i, h_theta in enumerate(h_thetad):
     print('h_theta' + unicode(i), '\t', h_theta)
@@ -47,8 +52,7 @@ for i, h_theta in enumerate(h_thetad):
 corr_train = metrics.r2(X_train, y_train, h_thetaf)
 print('R**2\t', corr_train)
 
-print('****Testing****')
-
+print('\n--Testing--')
 yp_test = lr.predict(X_test, h_thetaf)
 print(tabulate(list(zip(yp_test, y_test)), 
                 headers=['yp', 'yi'],
@@ -56,26 +60,35 @@ print(tabulate(list(zip(yp_test, y_test)),
 corr_test = metrics.r2(X_test, y_test, h_thetaf)
 print('R**2\t', corr_test)
 
-print('****Stochastic Gradient Descent****')
+print('\n\n****Stochastic Gradient Descent****')
+print('\n--Training--')
 alpha = 0.0
 h_thetaf, cost = lr.fit(partial(lr.ES, alpha=alpha),
                   partial(lr.gradES, alpha=alpha),
                   h_theta0, 
-                  eta=0.3,
-                  it_max=500, gf='sgd')(data)
+                  eta=0.1,
+                  it_max=1000, gf='sgd')(scaledtrain_data)
 
 lr.plot_cost(cost)
 print('Coefficients\t', h_thetaf)
-yp = lr.predict(X, h_thetaf)
+yp_train = lr.predict(X_train, h_thetaf)
 h_thetad = scale.denormalize(h_thetaf)
-print(tabulate(list(zip(yp, y)), 
+print(tabulate(list(zip(yp_train, y_train)), 
                 headers=['yp', 'yi'],
                 tablefmt='fancy_grid'))
 print('Coefficients\t', h_thetad)
 for i, h_theta in enumerate(h_thetad):
     print('h_theta' + unicode(i), '\t', h_theta)
 
-corr_test = metrics.r2(X, y, h_thetaf)
+corr_train = metrics.r2(X_train, y_train, h_thetaf)
+print('R**2\t', corr_train)
+
+print('\n--Testing--')
+yp_test = lr.predict(X_test, h_thetaf)
+print(tabulate(list(zip(yp_test, y_test)), 
+                headers=['yp', 'yi'],
+                tablefmt='fancy_grid'))
+corr_test = metrics.r2(X_test, y_test, h_thetaf)
 print('R**2\t', corr_test)
 
 print('****Numpy Solution****')
