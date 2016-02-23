62f98fdaea8192d1bb59f5d1dd59124be9d121d3
irislogex.py <philplessner@gmail.com>
2016-02-23 08:57:58 +0000

    added SGD

--- irislogex.py
+++ irislogex.py
@@ -1,8 +1,10 @@
 # coding: utf-8
 from __future__ import print_function, division, unicode_literals
 from toolz import pluck, compose
+from tabulate import tabulate
 from utility import csv_reader, Scaler, prepend_x0, dot
 import logistic_regression as logr
+import linear_regression as linr
 from ml_util import train_test_split
 
 
@@ -10,8 +12,6 @@ from ml_util import train_test_split
 # SL: sepal length, SW: Sepal Width, PL: Petal Length, PW: Petal Width
 # 0:  Iris Setosa 1: Iris Versicolour 2: Iris Virginica
 Z, q = csv_reader('./data/iris.csv', ['SL', 'SW', 'PL', 'PW'], 'Type')
-print(Z)
-print(q)
 # Get Sepal Length and Petal Length features
 Zp = list(pluck([0, 2], Z))
 # Get only the Iris Setosa (0) and Iris Versicolour (1) classes
@@ -35,26 +35,55 @@ scaled_train = zip(scaledX_train, y_train)
 h_theta0 = [1., 1., 1.]
 print('****Gradient Descent****')
 print('--Training--')
-h_thetaf, cost = logr.logistic_reg(logr.logistic_log_likelihood,
+h_thetaf, cost = linr.fit(logr.logistic_log_likelihood,
                                 logr.grad_logistic,
                                 h_theta0,
                                 scaled_train,
-                                it_max=500)
+                                eta=0.03,
+                                it_max=500,
+                                gf='gd')
 
 logr.plot_cost(cost)
 print(h_thetaf)
 
-for xi, yi in scaled_train:
-    ypi = logr.logistic(dot(h_thetaf, xi))
-    print(yi, ypi, round(ypi))
+yp_train = logr.predict(logr.logistic, scaledX_train, h_thetaf)
+print(tabulate(list(zip(yp_train, map(round, yp_train), y_train)), 
+                headers=['yp', 'Predicted Class', 'Actual Class'],
+                tablefmt='fancy_grid'))
 
 print('--Testing--')
 # Use the training statistics to scale the test data
 X_test, y_test = zip(*test_data)
 scaledX_test = transform(X_test)
 scaled_test = zip(scaledX_test, y_test)
-for xi, yi in scaled_test:
-    ypi = logr.logistic(dot(h_thetaf, xi))
-    print(yi, ypi, round(ypi))
+yp_test = logr.predict(logr.logistic, scaledX_test, h_thetaf)
+print(tabulate(list(zip(yp_test, map(round, yp_test), y_test)), 
+                headers=['yp', 'Predicted Class', 'Actual Class'],
+                tablefmt='fancy_grid'))
 
-                                
+print('\n\n****Stochastic Gradient Descent****')
+print('\n--Training--')
+h_thetaf, cost = linr.fit(logr.logistic_log_likelihood_i,
+                                logr.logistic_log_gradient_i,
+                                h_theta0,
+                                scaled_train,
+                                eta=0.5,
+                                it_max=500,
+                                gf='sgd')
+logr.plot_cost(cost)
+print(h_thetaf)
+
+yp_train = logr.predict(logr.logistic, scaledX_train, h_thetaf)
+print(tabulate(list(zip(yp_train, map(round, yp_train), y_train)), 
+                headers=['yp', 'Predicted Class', 'Actual Class'],
+                tablefmt='fancy_grid'))
+
+print('\n--Testing--')
+# Use the training statistics to scale the test data
+X_test, y_test = zip(*test_data)
+scaledX_test = transform(X_test)
+scaled_test = zip(scaledX_test, y_test)
+yp_test = logr.predict(logr.logistic, scaledX_test, h_thetaf)
+print(tabulate(list(zip(yp_test, map(round, yp_test), y_test)), 
+                headers=['yp', 'Predicted Class', 'Actual Class'],
+                tablefmt='fancy_grid'))
